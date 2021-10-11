826453aa2542a179719d3c791df0f330c24a8f4c
s24546 <91979151+s24546@users.noreply.github.com>
2021-10-12 00:14:03 +0200

    Add files via upload

---
+++ s01-mul-stdin.cpp
@@ -0,0 +1,7 @@
+auto main () -> int
+{
+auto const a = a s k _ u s e r _ f o r _ i n t e g e r (" a = ");
+auto const b = a s k _ u s e r _ f o r _ i n t e g e r (" b = ");
+std :: cout << ( a * b ) << "\ n ";
+return 0;
+}
