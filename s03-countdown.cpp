d327426cc789753e7b8ffd289e8b571586f71b20
s24546 <91979151+s24546@users.noreply.github.com>
2021-10-18 19:05:35 +0200

    Add files via upload

---
+++ s03-countdown.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main () 
+{   
+int a;
+std :: cin >> a;
+for (int i=a-1; i>=0; i--)
+{      
+std :: cout << i << std :: endl;
+}     
+   return 0;
+}
+
+
---
+++ s03-password.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+ int main()
+{
+std :: string p;
+do
+{
+std :: cout << "Podaj hasło" << std :: endl;
+std :: cin >> p;
+
+}while (p!="student"); 
+std :: cout << "OK!" << std :: endl;
+return 0;
+} 
+
+
+
