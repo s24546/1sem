f3cc883e4dcdb0b47c07228bb4902dd5d964682e
s24546 <91979151+s24546@users.noreply.github.com>
2021-10-18 19:35:34 +0200

    Add files via upload

---
+++ s03-beer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main () 
+{   
+for (int i=99; i>1; i--)
+{      
+std :: cout << i << "Bottles of beer on the wall," << std :: endl << i << "Bottles of beer.Take one down, pass it around" << std :: endl;
+}     
+std :: cout << "1 Bottle of beer on the wall," << std :: endl << "Bottles of beer.Take one down, pass it around" << std :: endl;
+std :: cout << "No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall...";   
+return 0;
+}
+
+
