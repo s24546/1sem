515163714681695734335f1f898328ea2fde334d
s24546 <91979151+s24546@users.noreply.github.com>
2021-10-29 13:52:02 +0200

    Update s01-mul-stdin.cpp

--- s01-mul-stdin.cpp
+++ s01-mul-stdin.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <string>
-
+int ask_user_for_integer()
+{
+    std::string a;
+    std::cin >> a;
+    return std::stoi(a);
+}
 int main ( int argc , char * argv [])
 {
 int a , b;
-std :: cout << "Podaj liczbę \n";
-std :: cin >> a;
-std :: cout << "Podaj drugą liczbę \n";
-std :: cin >> b; 
+std :: cout << "Podaj liczbe \n";
+a = ask_user_for_integer();
+std :: cout << "Podaj druga liczbe \n";
+b = ask_user_for_integer();
 std :: cout << "Wynik: \n";
 std :: cout << (a * b) << "\n";
 return 0;
 }
+
