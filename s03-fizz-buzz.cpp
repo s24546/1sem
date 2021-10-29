41a343fc735d811d87920ebb73e45b0c7ad8c37f
s24546 <91979151+s24546@users.noreply.github.com>
2021-10-29 14:33:02 +0200

    Update s03-fizz-buzz.cpp

--- s03-fizz-buzz.cpp
+++ s03-fizz-buzz.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 
-int main () 
-{   
-int a;
-std :: cin >> a;
-for (int i=1; i<=a; i++)
-{      
-std :: cout << i << std :: endl;
-if (i%3==0)
+int main ()
 {
-std :: cout << "FIZZ" << std :: endl;
-}   
-if (i%5==0)
-{
-std :: cout << "BUZZ" << std :: endl;
-}
-}
-return 0;
+    int a;
+    std :: cin >> a;
+    for (int i=1; i<=a; i++)
+    {
+
+        if(i%3==0 && i%5==0)
+        {
+             std :: cout << i << std :: endl;
+            std :: cout << "FIZZBUZZ" << std :: endl;
+        }
+        else if (i%3==0)
+        {
+             std :: cout << i << std :: endl;
+            std :: cout << "FIZZ" << std :: endl;
+        }
+        else if (i%5==0)
+        {
+             std :: cout << i << std :: endl;
+            std :: cout << "BUZZ" << std :: endl;
+        }
+    }
+    return 0;
 }
