b918adc4b37b3e601da637fe89bb4a3c8c1b2bc7
s24546 <91979151+s24546@users.noreply.github.com>
2021-11-05 13:26:32 +0100

    Update s03-beer.cpp

--- s03-beer.cpp
+++ s03-beer.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
-int main () 
-{   
+int main ( int argc , char * argv [])
+{
 for (int i=99; i>1; i--)
-{      
-std :: cout << i << "Bottles of beer on the wall," << std :: endl << i << "Bottles of beer.Take one down, pass it around" << std :: endl;
-}     
-std :: cout << "1 Bottle of beer on the wall," << std :: endl << "Bottles of beer.Take one down, pass it around" << std :: endl;
-std :: cout << "No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall...";   
+{
+std :: cout << i << "Bottles of beer on the wall, \n" <<"\n"<< i << "Bottles of beer.Take one down, pass it around \n" << "\n";
+}
+std :: cout << "1 Bottle of beer on the wall, \n" << std :: endl << "Bottles of beer.Take one down, pass it around \n" << "\n";
+std :: cout << "No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall... \n";
 return 0;
 }
-
 
