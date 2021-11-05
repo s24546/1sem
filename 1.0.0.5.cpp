ea51baaab74893fa565539b4399a1bf3939b0fa9
s24546 <91979151+s24546@users.noreply.github.com>
2021-11-05 14:25:56 +0100

    Add files via upload

---
+++ 1.0.0.5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main ()
+{
+double a;
+std :: cin >> a;
+if (a>=0)
+std :: cout <<"|"<<a<<"|="<<a<<"\n";
+else
+std :: cout <<"|"<<a<<"|="<<-a<<"\n";
+
+return 0;
+}
