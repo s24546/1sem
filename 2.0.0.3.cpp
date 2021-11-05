71213514e98970fbc554424370ca4a00def4bcad
s24546 <91979151+s24546@users.noreply.github.com>
2021-11-05 14:58:49 +0100

    Update 2.0.0.3.cpp

--- 2.0.0.3.cpp
+++ 2.0.0.3.cpp
@@ -2,12 +2,21 @@
 
 int main ()
 {
-int a, b;
-std :: cin >> a;
-std :: cin >> b;
-for (int i=a; i<b; i=i+a)
-{
-std :: cout << i << std :: endl;
-}
-   return 0;
-}
+    int a, b, s;
+    std :: cin >> a;
+    std :: cin >> b;
+    std :: cin >> s;
+    for (int i=a; i<b; i=i+s)
+    {
+        if (s==0)
+        {
+            std :: cout <<"Trzecia liczba nie moga byc zerem \n";
+            break;
+        }
+        else
+        {
+            std :: cout << i << std :: endl;
+        }
+    }
+        return 0;
+    }
