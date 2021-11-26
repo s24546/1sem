fb9990303446bc14c49693d7b3bd92e43325e63e
s24546 <91979151+s24546@users.noreply.github.com>
2021-11-26 14:58:35 +0100

    Create 3.1.0.1.cpp

---
+++ 3.1.0.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+auto init(int a[], int n) -> void
+{
+     for(int i=0;i<=n;i++)
+    {
+     a[i-1]=0;
+    }
+}
+
+
+int main()
+{
+    int rozm;
+    int a[50000];
+    std::cout<<"Podaj rozmiar tablicy: ";
+    std::cin>>rozm;
+   init(a,rozm);
+return 0;
+}
