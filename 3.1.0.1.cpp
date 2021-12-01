c46e2ce3f3f60cd08a886461a05da0ba923c54a5
s24546 <91979151+s24546@users.noreply.github.com>
2021-12-01 21:19:44 +0100

    Update 3.1.0.1.cpp

--- 3.1.0.1.cpp
+++ 3.1.0.1.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-
-auto init(int a[], int n) -> void
+auto init(int a[], int n, int start) -> void
 {
      for(int i=0;i<=n;i++)
     {
      a[i-1]=0;
-    }
 }
-
-
+}
 int main()
 {
     int rozm;
     int a[50000];
     std::cout<<"Podaj rozmiar tablicy: ";
     std::cin>>rozm;
-   init(a,rozm);
+    for(int i=0;i<rozm;i++)
+   {
+       std::cout<<a[i]<<"\n";
+   }
 return 0;
 }
