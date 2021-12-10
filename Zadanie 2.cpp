9f9a81d5009705b168f2ca23164a4f6fdd4fb477
s24546 <91979151+s24546@users.noreply.github.com>
2021-12-10 13:44:20 +0100

    Create Zadanie 2.cpp

---
+++ Zadanie 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+auto main() -> int
+{
+    int  wzrost;
+    double waga;
+    auto str = std::string("Andrzej");
+    wzrost=175;
+    waga=80;
+    std :: cout <<"Imie:"<<str<<std::endl;
+    std :: cout <<"Wzrost:"<<wzrost<<"cm"<<"\n";
+    std :: cout <<"Waga:"<<waga<<"kg"<<std::endl;
+    return 0;
+}
