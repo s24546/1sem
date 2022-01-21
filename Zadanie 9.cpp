8d7cea9f0415c15c7917a860acc0b0ddeb1df324
s24546 <91979151+s24546@users.noreply.github.com>
2022-01-21 14:10:21 +0100

    Update Zadanie 9.cpp

--- Zadanie 9.cpp
+++ Zadanie 9.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-auto is_palindrome(std::string d) -> void
+auto is_palindrome(std::string d) -> bool
 {
     std::string a;
     a=d;
@@ -9,17 +9,18 @@ auto is_palindrome(std::string d) -> void
     {
         auto tmp = d[i];
         d[i] = d[d.size() - 1 - i];
+        d[d.size() - 1 - i] = tmp;
     }
     std::cout<<d<<"\n";
 
     if (a==d)
     {
-        std::cout<<"true";
+        return true;
 
     }
     else
     {
-        std::cout<<"false";
+        return false;
     }
 
 }
@@ -28,5 +29,12 @@ int main()
     std::string y;
     std::cout<<"Podaj slowo"<<"\n";
     std::cin>> y;
-    is_palindrome(y);
+    if(is_palindrome(y))
+    {
+        std::cout<< "to slowo jest palindromem \n";
+    }
+    else
+    {
+        std::cout<<"to slowo nie jest palindromem \n";
+    }
 }
