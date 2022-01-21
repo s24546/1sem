67b36f74f32ac24ae0489e6e9787121dc4e3d507
s24546 <91979151+s24546@users.noreply.github.com>
2022-01-21 12:41:43 +0100

    Update Zadanie 5.cpp

--- Zadanie 5.cpp
+++ Zadanie 5.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-auto is_even(int n) -> void
+auto is_even(int n) -> bool
 {
     if(n%2==0)
     {
-        std::cout<<"true"<<"\n";
+        return true;
     }
-    else
-    {
-        std::cout<<"false"<<"\n";
-    }
+    return false;
 }
 int main()
 {
@@ -16,4 +13,13 @@ int main()
     std::cout<<"Podaj liczbe:"<<"\n";
     std::cin>>b;
     is_even(b);
+    if (is_even(b)==true)
+    {
+        std::cout<<"Liczba jest parzysta"<<"\n";
+    }
+    else
+    {
+        std::cout<<"Liczba jest niepazysta"<<"\n";
+    }
     return 0;
+}
